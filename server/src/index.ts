ee90aa02a474679732eec7825f100bd6e80650ff
Agustin Pivetta <aguspivetta@gmail.com>
2022-06-22 21:03:19 +0200

    Initial client setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,5 +4,5 @@ const port = 4000
 const app = createApp()
 
 app.listen(port, () => {
-  console.log(`Heni challenge - Prints server listening on port ${port}`)
+  console.log(`HENI challenge - Prints server listening on port ${port}`)
 })
